435d97f6525b9c64aa257afd6a51b48435dc82bf
wcnoname5 <wcnoname5@gmail.com>
2023-04-02 20:46:56 +0800

    modify useful_functions.py and add cypher_script.py file

--- W1_HW/python_scripts/useful_functions.py
+++ W1_HW/python_scripts/useful_functions.py
@@ -7,12 +7,22 @@ def encrypt_letter(letter, key):
     return(chr(enc_code))
 
 
-def decrypth_letter(letter, key):
+def decrypt_letter(letter, key):
     enc_l_code = ord(letter)
     k_code = ord(key)
     dec_code = enc_l_code - k_code
     return(chr(dec_code))
 
 
-def process_message(message, key, encrypt):
-    print("help")
+def process_message(message, key, encrypt = True):
+    #enc_list = [list(message), list(key)]
+    list_k = list(key)
+    output = str()
+    for index, mes_let  in enumerate(message):
+        i = index % len(list_k) 
+        if encrypt:
+            k = encrypt_letter(mes_let, list_k[i])
+        else:
+            k = decrypt_letter(mes_let, list_k[i])
+        output = output + k
+    return(output)
