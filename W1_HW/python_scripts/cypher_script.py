91437a353bf11509db68789f92f6573cb48f3fcc
wcnoname5 <wcnoname5@gmail.com>
2023-04-03 14:30:44 +0800

    1. modify cypher_script.py. \n 2.add caches and verification text files.

--- W1_HW/python_scripts/cypher_script.py
+++ W1_HW/python_scripts/cypher_script.py
@@ -5,9 +5,9 @@ import argparse
 
 #if __name__ == "__main__":
 praser = argparse.ArgumentParser(__file__)
-praser.add_argument("--message", action = "store", dest ="mes", required=True,
+praser.add_argument("-m", "--message", action = "store", dest ="mes", required=True,
                     help="Add a text file or a path of it")
-praser.add_argument("--key", action="store", dest="key_string", required=True,
+praser.add_argument("-k", "--key", action="store", dest="key_string", required=True,
                     help="")
 praser.add_argument("--mode", action="store", dest="mode", required=True,
                     #choices=["enc","dec"],
@@ -17,12 +17,12 @@ arg = praser.parse_args()
 
 if arg.mode == "enc":
     enc=True #encryption
-    file_name = "msg_file_encrypted.txt"
+    suffix = "_encrypted.txt"
 else:
-    enc=False #
-    file_name = "msg_file_decrypted.txt"
-
-
+    enc=False 
+    suffix = "_decrypted.txt"
+temp = arg.mes.split('.')[0] #w/o file format
+file_name = temp + suffix
 message = open(arg.mes, 'r') #open the message file
 
 word_list = list(message)
@@ -32,7 +32,7 @@ for i in word_list:
     out_content = useful_functions.process_message(i, arg.key_string, enc)
     f.write(out_content)
     f.write('\n')
-    print(out_content)
+    #print(out_content)
 f.close()
 
 #write out output file
