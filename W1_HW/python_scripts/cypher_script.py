da9265212eb2073f1ff80566165c70ff2521488b
wcnoname5 <wcnoname5@gmail.com>
2023-04-07 22:22:17 +0800

    Modify cypher_script.py and finish HW of python script

--- W1_HW/python_scripts/cypher_script.py
+++ W1_HW/python_scripts/cypher_script.py
@@ -1,38 +1,40 @@
 import useful_functions
 import argparse
 
-#output msg_file_encrypted.txt/msg_file_decrypted.txt
-
-if __name__ == "__main__":
-    praser = argparse.ArgumentParser(__file__)
-    praser.add_argument("-m", "--message", action = "store", dest ="mes", required=True,
-                        help="Add a text file or a path of it")
-    praser.add_argument("-k", "--key", action="store", dest="key_string", required=True,
-                        help="")
-    praser.add_argument("--mode", action="store", dest="mode", required=True,
-                        #choices=["enc","dec"],
-                        help="Specify \"enc\" or \"dec\" for encryption or decryption")
-    arg = praser.parse_args()
-    
-    
-    if arg.mode == "enc":
+def main(message_text, key, mode):
+    if mode == "enc":
         enc=True #encryption
-        suffix = "_encrypted.txt"
+        suffix = "encrypted"
     else:
         enc=False 
-        suffix = "_decrypted.txt"
-    temp = arg.mes.split('.')[0] #w/o file format
-    file_name = temp + suffix
-    message = open(arg.mes, 'r') #open the message file
-    
+        suffix = "decrypted"
+
+    temp = message_text.split('.')[0] #w/o file format
+    file_name = temp + "_" + suffix + ".txt"
+    message = open(message_text, 'r') #open the message file
     word_list = list(message)
-    
+
     f = open(file_name, 'w')# write
     for i in word_list:
-        out_content = useful_functions.process_message(i, arg.key_string, enc)
+        out_content = useful_functions.process_message(i, key, enc)
         f.write(out_content)
         f.write('\n')
         #print(out_content)
     f.close()
+    print("Message is successfully " + suffix +"!")
 
-#write out output file
+def argparse_func():
+    praser = argparse.ArgumentParser(__file__)
+    praser.add_argument("-m", "--message", action = "store", dest ="mes", required=True,
+                        help="Add a text file or a path of it")
+    praser.add_argument("-k", "--key", action="store", dest="key", required=True,
+                        help="Type your encryption/decryption key")
+    praser.add_argument("--mode", action="store", dest="mode", required=True,
+                        choices=["enc","dec"],
+                        help="Specify \"enc\" or \"dec\" for encryption or decryption")
+    args = praser.parse_args()
+    return(args)
+arg = argparse_func()
+
+if __name__ == "__main__":
+    main(arg.mes, arg.key, arg.mode)
